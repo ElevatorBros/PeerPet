b83b058dada9330fefa51a9180fa2d3171d22965
Joshua Bourgeot <dragonsploder@gmail.com>
2023-02-25 15:24:55 -0500

    Basic Display Stuff

--- src/display.go
+++ src/display.go
@@ -4,6 +4,7 @@ import (
     "log"
     "github.com/gdamore/tcell/v2"
     "github.com/bennicholls/burl-E/reximage"
+    "unicode/utf8"
 )
 
 
@@ -18,20 +19,27 @@ func quit_tcell() {
     }
 }
 
-func draw_xp_image(image reximage.ImageData) {
+func draw_xp_image(offsetX int, offsetY int, image reximage.ImageData) {
     for x := 0; x < image.Width; x++ {
         for y := 0; y < image.Height; y++ {
+            cell, _ := image.GetCell(x, y)
+            forground_cell_color := tcell.NewRGBColor(int32(cell.R_f), int32(cell.G_f), int32(cell.B_f))
+            background_cell_color := tcell.NewRGBColor(int32(cell.R_b), int32(cell.G_b), int32(cell.B_b))
+            
+            color_style := tcell.StyleDefault.Background(background_cell_color).Foreground(forground_cell_color)
+            cell_rune, _ := utf8.DecodeRuneInString(string(cell.Glyph))
+            display.SetContent(offsetX + x, offsetY + y, cell_rune, nil, color_style)
         }
     }
 }
 
 func draw() {
-    image, err := reximage.Import("./rec/test.xp")
+    image, err := reximage.Import("./rec/pet1.xp")
 	if err != nil {
 		log.Fatalf("%+v", err)
 	}
 
-    draw_xp_image(image)
+    draw_xp_image(0, 0, image)
     display.Show()
 }
 
--- src/main.go
+++ src/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	//"fmt"
-    "time"
+    "github.com/gdamore/tcell/v2"
 )
 
 func main() {
 	//fmt.Printf("no errors")
 
     setup_tcell()
-    hello()
     draw()
-    time.Sleep(1000 * time.Millisecond)
-    quit_tcell()
+
+    for {
+        ev := display.PollEvent()
+        switch ev := ev.(type) {
+            case *tcell.EventKey:
+                if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+                    quit_tcell()
+                    return;
+                }
+            case *tcell.EventResize:
+                display.Sync()
+        }
+    }
 }
