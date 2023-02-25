ca4e2065ab57399a5494257f22e453ec6b13e232
Ronan Maharaj <admin@ronanm.me>
2023-02-25 14:33:26 -0500

    return filename

--- src/daemon.go
+++ src/daemon.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
 )
 
@@ -9,13 +10,15 @@ func dMain() {
 	CreateDataDir()
 }
 
-func CreateDataDir() {
+func CreateDataDir() string {
 	xdg_data := os.Getenv("XDG_DATA_HOME")
 	if xdg_data == "" {
 		xdg_data = "~/.local/share"
 	}
+    xdg_data = fmt.Sprintf("%s/peerpet", xdg_data)
 	folder, err := os.Stat(xdg_data)
 	if errors.Is(err, os.ErrNotExist) {
 		os.Mkdir(folder.Name(), os.ModeDir)
 	}
+    return fmt.Sprintf("%s/pets.json", xdg_data)
 }
