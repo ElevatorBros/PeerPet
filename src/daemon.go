5e6a09cb6f9cafe6c97b5865694cb2165624e141
Ronan Maharaj <admin@ronanm.me>
2023-02-25 14:23:12 -0500

    i can make directories now

--- src/daemon.go
+++ src/daemon.go
@@ -3,12 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 )
 
-func dMain() {
+func main() {
     createDataDir()
 }
 
@@ -17,32 +15,9 @@ func createDataDir() {
     if xdg_data == "" { 
       xdg_data = "~/.local/share" 
     }
+    xdg_data = fmt.Sprintf("%s/peerpet", xdg_data)
     folder, err := os.Stat(xdg_data)
     if errors.Is(err, os.ErrNotExist) {
-        if err := os.Mkdir(folder.Name(), os.ModePerm); err != nil {
-            log.Fatalln("~/.local/share does not exist! Manually create it")
-        }
-    }
-    if !folder.IsDir() {
-      log.Fatalf("Folder is not directory")
-    }
-
-    var pet Pet
-    readPets(fmt.Sprintf("%s/pets.json", xdg_data), &pet)
-}
-
-func readPets(filename string, pet *Pet) {
-    f, err := os.Open(filename) 
-    defer f.Close()
-    if err != nil {
-        log.Fatalf("Error reading pets.json file")
-    }
-    b, err := ioutil.ReadAll(f)
-    if err != nil {
-        log.Fatalf("Error reading pets.json file")
-    }
-    err = Unjsonify(b, pet)
-    if err != nil  {
-        log.Fatalf("Error reading json data file")
+        os.Mkdir(folder.Name(), os.ModeDir)
     }
 }
