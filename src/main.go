3907cf0d273aad960b62aec092b0736e926b74ec
Joshua Bourgeot <dragonsploder@gmail.com>
2023-02-25 19:23:28 -0500

    pet7

--- src/main.go
+++ src/main.go
@@ -4,27 +4,27 @@ func main() {
 	//a := NewRelay()
 	//a.joinRoom("aaabb44")
 
-	//RunGUI()
+	RunGUI()
 	//creates pet named john
-	pet := NewPet("john")
-	pet1 := NewPet("ronan")
-	pet2 := NewPet("dog")
-	//makes sure storing directory exists and returns path
-	path := CreateDataDir()
+	//pet := NewPet("john")
+	//pet1 := NewPet("ronan")
+	//pet2 := NewPet("dog")
+	////makes sure storing directory exists and returns path
+	//path := CreateDataDir()
 
-	//serializes pet to json
-	err := WritePetToJson(pet, path)
-	WritePetToJson(pet1, path)
-	WritePetToJson(pet2, path)
-	if err != nil {
-		panic(err)
-	}
+	////serializes pet to json
+	//err := WritePetToJson(pet, path)
+	//WritePetToJson(pet1, path)
+	//WritePetToJson(pet2, path)
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	//reads stored json files to pet array
-	pets := ReadPets(path)
+	////reads stored json files to pet array
+	//pets := ReadPets(path)
 
-	//prints pet array data
-	for _, thing := range pets {
-		thing.Print()
-	}
+	////prints pet array data
+	//for _, thing := range pets {
+	//	thing.Print()
+	//}
 }
